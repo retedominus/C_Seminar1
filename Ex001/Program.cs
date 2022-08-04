1ecac9c033b3828fad00d6d5be0a61248d64f9ad
Maksim Sinikov <md.sinikov@gmail.com>
2022-08-05 00:01:47 +0500

    Единственный комит

---
+++ Ex001/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+if(value == 1)
+{
+    Console.WriteLine("Сегодня ПОНЕДЕЛЬНИК");
+}
+else if(value == 2)
+{
+    Console.WriteLine("Сегодня ВТОРНИК");
+}
+else if(value == 3)
+{
+    Console.WriteLine("Сегодня СРЕДА");
+}
+else if(value == 4)
+{
+    Console.WriteLine("Сегодня ЧЕТВЕРГ");
+}
+else if(value == 5)
+{
+    Console.WriteLine("Сегодня ПЯТНИЦА");
+}
+else if(value == 6)
+{
+    Console.WriteLine("Сегодня СУББОТА");
+}
+else if(value == 7)
+{
+    Console.WriteLine("Сегодня ВОСКРЕСЕНЬЕ");
+}
+else
+{
+    Console.WriteLine("Вы вообще не то число ввели, досвиданья!");
+}
---
+++ Ex003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int count = value * (-1);
+
+while(count <= value)
+{
+    Console.Write($"{count} ");
+    count++;
+}
